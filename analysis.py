9b05e8022dfc5cb759abd2799f193c5104816561
Eric Yao <ericyaohan@yahoo.com>
2026-08-22 21:30:10 -0400

    need to rerun cosine and inverse

--- analysis.py
+++ analysis.py
@@ -12,12 +12,22 @@ from calculations import  calculate_pass_k, calculate_entropy_k
 K = [1, 2, 4, 8, 16, 32]
 N = 32
 
-def stitch_data():
+def stitch_executed_results():
+    records = []
+
+    for path in glob.glob("results/execution_results_*.json"):
+        name = os.path.basename(path)
+        dataset = re.match(r"execution_results_(\w+).json", name).groups()
+        print(dataset)
+        data = json.load(open(path))
+
+        print(data.keys())
+
+def stitch_results():
     records = []
     for path in glob.glob("results/results_*_*_batch*.json"):
         name  = os.path.basename(path)
         dataset, seed, batch = re.match(r"results_(\w+)_(\d+)_batch(\d+)\.json", name).groups()
-        print(path)
         data = json.load(open(path))
         for method, d in data["method"].items():
             for index, c, answers, in zip(d['problem_indices'], d['c_counts'], d['answers']):
@@ -95,20 +105,26 @@ def graph_pass_k_data(per_seed):
     
 
 if __name__ == "__main__":
+    stitch_executed_results()
 
-    df = stitch_data()
-    print(len(df), np.asarray(df['answers'].iloc[0]).shape)
-    for k in K:
-        df[f"pass@{k}"] = df['c'].apply(lambda c : calculate_pass_k(N, k, c))
-        df[f"entropy@{k}"] = df['answers'].apply(lambda a : calculate_entropy_k(a, k))
+    # df = stitch_results()
 
-    cols = [f'pass@{k}' for k in K] + [f'entropy@{k}' for k in K]
-    per_seed = df.groupby(["dataset", "seed", "method"])[cols].mean()   
+    # for k in K:
+    #     df[f"pass@{k}"] = df['c'].apply(lambda c : calculate_pass_k(N, k, c))
+    #     df[f"entropy@{k}"] = df['answers'].apply(lambda a : calculate_entropy_k(a, k))
+
+    # cols = [f'pass@{k}' for k in K] + [f'entropy@{k}' for k in K]
+    # per_seed = df.groupby(["dataset", "seed", "method"])[cols].mean()   
     
-    summary = per_seed.groupby(["dataset", "method"]).agg(["mean", "std"])
-    print(summary.round(3))
+    # summary = per_seed.groupby(["dataset", "method"]).agg(["mean", "std"])
+    # print(summary.round(3))
 
-    graph_pass_k_data(per_seed)
-    graph_entropy_k_data(per_seed)
+    # # load in humaneval and mbpp
+
+
+
+
+    # graph_pass_k_data(per_seed)
+    # graph_entropy_k_data(per_seed)
 
 
